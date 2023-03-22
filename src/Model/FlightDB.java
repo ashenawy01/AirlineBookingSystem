3aa6a791d5a395641c81a49c92f4b31676ba02fd
BUEdevelopers <web.project22cs@outlook.com>
2023-03-22 23:04:29 +0200

    moutasem

--- src/Model/FlightDB.java
+++ src/Model/FlightDB.java
@@ -1,4 +1,11 @@
 package Model;
 
+import java.time.LocalDateTime;
+
 public class FlightDB {
+    private int flightID;
+    private String origin;
+    private String destination;
+    private LocalDateTime flightTime;
+    private float duration;
 }
