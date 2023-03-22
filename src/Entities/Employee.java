1f06d225ad6a0a52514130c85dc3e6dd8a2cc8c8
7oudaHA <mahmoud.hussein.massoud@gmail.com>
2023-03-22 23:04:06 +0200

    Mahmoud 221523

--- src/Entities/Employee.java
+++ src/Entities/Employee.java
@@ -17,4 +17,48 @@ public abstract class Employee {
         this.email = email;
         this.password = password;
     }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Timestamp getCreatedAT() {
+        return createdAT;
+    }
+
+    public int getCreatedByID() {
+        return createdByID;
+    }
 }
