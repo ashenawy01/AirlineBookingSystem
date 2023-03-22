4bdc07f8f17472deff52c8ebe53ac9a8b6154205
7oudaHA <mahmoud.hussein.massoud@gmail.com>
2023-03-22 23:39:24 +0200

    Mahmoud 221523

--- src/Entities/Staff.java
+++ src/Entities/Staff.java
@@ -32,4 +32,9 @@ public class Staff extends  Employee{
         this.department = department;
     }
 
+
+    /* add a flight to the array */
+    public void addFlight(Flight flight){
+     managedFlights.add(flight);
+    }
 }
