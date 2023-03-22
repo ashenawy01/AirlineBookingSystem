af385d49cce5436014db80ef5dd5f28cce601b8d
7oudaHA <mahmoud.hussein.massoud@gmail.com>
2023-03-22 23:13:09 +0200

    Mahmoud 221523

---
+++ src/Entities/Admin.java
@@ -0,0 +1,28 @@
+package Entities;
+
+public class Admin extends Employee {
+private boolean isGlobal;
+private  boolean isActive;
+
+    public Admin(String firstName, String lastName, String email, String password, boolean isGlobal, boolean isActive) {
+        super(firstName, lastName, email, password);
+        this.isGlobal = isGlobal;
+        this.isActive = isActive;
+    }
+
+    public boolean isGlobal() {
+        return isGlobal;
+    }
+
+    public void setGlobal(boolean global) {
+        isGlobal = global;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
