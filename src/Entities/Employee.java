4462474551e359e8142603a7864fedbe9c79f628
7oudaHA <mahmoud.hussein.massoud@gmail.com>
2023-03-22 23:00:01 +0200

    Mahmoud 221523

--- src/Entities/Employee.java
+++ src/Entities/Employee.java
@@ -2,7 +2,7 @@ package Entities;
 
 import java.sql.Timestamp;
 
-public class Employee {
+public abstract class Employee {
     private int ID;
     private String firstName;
     private String lastName;
@@ -17,49 +17,4 @@ public class Employee {
         this.email = email;
         this.password = password;
     }
-
-    public int getID() {
-        return ID;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public Timestamp getCreatedAT() {
-        return createdAT;
-    }
-
-    public int getCreatedByID() {
-        return createdByID;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
 }
