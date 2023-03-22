2e54083515f7f4663354a7c247b556eb77462c2c
7oudaHA <mahmoud.hussein.massoud@gmail.com>
2023-03-22 23:31:20 +0200

    Mahmoud 221523

--- src/Entities/Staff.java
+++ src/Entities/Staff.java
@@ -4,5 +4,32 @@ import java.util.ArrayList;
 
 public class Staff extends  Employee{
     private String jobTitle;
+    private Department department;
     private ArrayList<Flight> managedFlights;
+
+    public Staff(String firstName, String lastName, String email, String password, ArrayList<Flight> managedFlights) {
+        super(firstName, lastName, email, password);
+        this.managedFlights = managedFlights;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public ArrayList<Flight> getManagedFlights() {
+        return managedFlights;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
 }
