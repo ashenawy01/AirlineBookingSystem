54824102d144c3c3f1a17997f02c4f0e629a171f
BUEdevelopers <web.project22cs@outlook.com>
2023-03-22 23:26:08 +0200

    moutasem

---
+++ src/Entities/Seat.java
@@ -0,0 +1,34 @@
+package Entities;
+
+import com.sun.jdi.ClassType;
+
+public class Seat {
+    private String seatNumber;
+    private ClassType classType;
+    private boolean isBooked;
+
+    public Seat(String seatNumber, ClassType classType) {
+        this.seatNumber = seatNumber;
+        this.classType = classType;
+        isBooked = false;
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public ClassType getClassType() {
+        return classType;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public void setClassType(ClassType classType) {
+        this.classType = classType;
+    }
+
+    public void book(){}
+    public void cancelBook(){};
+}
