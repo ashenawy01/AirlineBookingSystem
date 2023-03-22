f47897aa9a03684d805e374d90caa0774256b854
7oudaHA <mahmoud.hussein.massoud@gmail.com>
2023-03-22 22:46:04 +0200

    Mahmoud 221523

--- src/Entities/Client.java
+++ src/Entities/Client.java
@@ -1,10 +1,11 @@
 package Entities;
 
+import java.util.ArrayList;
+
 public class Client {
     private  int id;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
-
 }
---
+++ src/Entities/Employee.java
@@ -0,0 +1,20 @@
+package Entities;
+
+import java.sql.Timestamp;
+
+public class Employee {
+    private int ID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Timestamp createdAT;
+    private int createdByID;
+
+    public Employee(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+}
