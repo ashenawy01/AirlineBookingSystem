07f44818ec302879985521dece9c48e3acf4aacb
Alshal <abdallah224005@bue.edu.eg>
2023-03-22 23:18:49 -0700

    224005

--- src/Entities/Client.java
+++ src/Entities/Client.java
@@ -1,11 +1,42 @@
 package Entities;
 
+
 import java.util.ArrayList;
 
-public class Client {
+public abstract class Client {
     private  int id;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setBooking(ArrayList<String> booking) {
+        this.booking = booking;
+    }
+
+    ArrayList<String> booking = new ArrayList<String>();
+
+    public Client(int id) {
+        this.id = id;
+    }
 }
