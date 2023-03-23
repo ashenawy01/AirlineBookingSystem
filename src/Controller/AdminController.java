cc91fdb518781646ece48e9e5fbd0d15866c7fba
Noureldin <noor.moataz.lashin@gmail.com>
2023-03-23 11:43:53 +0200

    Noureldin213527

--- src/Controller/AdminController.java
+++ src/Controller/AdminController.java
@@ -1,4 +1,47 @@
 package Controller;
-
+import Model.AdminDB;
+import Model.StaffDB;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
 public class AdminController {
+
+    private AdminDB admindb;
+    private StaffDB staffdb;
+
+
+    public AdminController() {
+    }
+
+    //Function to sign in for only admins
+    //it will return admin object
+    public AdminDB signin(String Email, String pass) {
+        //first...the email and the password should be not null
+        if (Email != null || pass != null) {
+            if (isValid(Email)) {
+                if (admindb.findAccount(Email, pass) == null) {
+                    System.out.println("this email is not exist, please try again");
+                    return null;
+                } else return null;
+            } else {
+                System.out.println("Error");
+                return null;
+            }
+        } else {
+            System.out.println("Error");
+            return null;
+        }
+    }
+
+    //to check the validation of the email
+    public static boolean isValid(String Email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        return pat.matcher(Email).matches();
+    }
 }
+
