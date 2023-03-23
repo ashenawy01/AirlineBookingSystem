6007f472af0ae32d4704e98a704d559d9a3c67b3
Noureldin <noor.moataz.lashin@gmail.com>
2023-03-23 14:51:08 +0200

    Noureldin213527

--- src/Controller/AdminController.java
+++ src/Controller/AdminController.java
@@ -15,33 +15,72 @@ public class AdminController {
     //Function to sign in for only admins
     //it will return admin object
     public Admin signin(String Email, String pass) {
-        Admin admin;
+        AdminDB admindb=new AdminDB();
+        ArrayList<Object> staff;
         //first...the email and the password should be not null
-        if (Email != null || pass != null) {
-                if (admindb.findAccount(Email, pass) == null) {
-                    return null;
-                } else {
-                    admin = (Admin) admindb.findAccount(Email, pass);
-                    return admin;
-                }
-            }else return null;
+        Admin admin=(Admin) admindb.findAccount(Email,pass);
+        if(admin != null){
+            System.out.println("Welcome " + admin.getFirstName());
+            System.out.println("\nYour employee");
+            staff=admindb.retrieveAll();
+            for (int i=0; i<staff.size()-1; i++){
+                System.out.println(staff.get(i));
+            }
+            return admin;
+        }
+        else {System.out.println("Incorrect username or password");
+            return null;}
 
     }
-
     public Admin CreateAdmin(String firstName, String lastName, String email, String password, boolean isGlobal, boolean isActive){
-        Admin admin = new Admin( firstName,  lastName,  email,  password,  isGlobal,  isActive);
-        return admin;
+        if(firstName.isEmpty()|| firstName.length()<2){
+            System.out.println("Error! Please, Enter a valid name");
+            return null;
+        }else if(lastName.isEmpty()|| lastName.length()<2){
+            System.out.println("Error! Please, Enter a valid name");
+            return null;
+        }else if (email==null){
+            System.out.println("Error! Please, Enter a valid Email");
+            return null;
+        } else if (password.isEmpty() || password.length() < 3 || !((password.matches(".*[a-zA-Z].*") && password.matches(".*\\d.*")))) {
+            System.out.println("Error! Please, Enter a valid pass (more than 6 char, includes chars and numbers)");
+            return null;
+        }else {
+            Admin admin=new Admin( firstName, lastName, email, password,isGlobal, isActive);
+            if(admindb.addObject(admin,true)){
+                System.out.println("welcome admin"+" "+admin.getFirstName());
+                return admin;
+            }
+            else{
+                System.out.println("Error with database connection, please try again");
+                return null;
+            }
+        }
     }
-
-
     public Staff CreateStaff(String firstName, String lastName, String email, String password, String jobTitle, Department department, ArrayList<Flight> managedFlights){
-
+        if(firstName.isEmpty()|| firstName.length()<2){
+            System.out.println("Error! Please, Enter a valid name");
+            return null;
+        }else if(lastName.isEmpty()|| lastName.length()<2){
+            System.out.println("Error! Please, Enter a valid name");
+            return null;
+        }else if (email==null){
+            System.out.println("Error! Please, Enter a valid Email");
+            return null;
+        } else if (password.isEmpty() || password.length() < 3 || !((password.matches(".*[a-zA-Z].*") && password.matches(".*\\d.*")))) {
+            System.out.println("Error! Please, Enter a valid pass (more than 6 char, includes chars and numbers)");
+            return null;
+        } else {
             Staff staff = new Staff( firstName,lastName,email, password, jobTitle, department, managedFlights);
-            return staff;
-    }
+            if(staffdb.addObject(staff,true)){
+                System.out.println("added successfully");
+                return staff;
+            }else System.out.println("Error with database connection, please try again");
+            return null;
 
 
-
+        }
+        }
     public boolean DeleteEmployee(int empID,boolean isAdmin){
         AdminDB adminDB=new AdminDB();
         StaffDB staffDB = new StaffDB();
@@ -56,8 +95,5 @@ public class AdminController {
         }else return false; // in case admin account is not global
         }else return false; // in case account is not found
     }
-
-
-
 }
 
