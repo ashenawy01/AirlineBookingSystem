fadb45726774638c5ea82427762e43c2bf06cae5
abdelrhman <abdelrhman225328@bue.edu.eg>
2023-03-23 15:42:19 +0200

    552

--- src/Controller/AdminController.java
+++ src/Controller/AdminController.java
@@ -7,31 +7,28 @@ public class AdminController {
 
     private AdminDB admindb;
     private StaffDB staffdb;
+    private Admin admin;
 
 
-    public AdminController() {
+    public AdminController(Admin admin) {
+        this.admin = admin;
     }
 
     //Function to sign in for only admins
     //it will return admin object
     public Admin signin(String Email, String pass) {
         AdminDB admindb=new AdminDB(); // a new object of adminDB is created
-        ArrayList<Object> staff; // an arraylist of staff is created
         //first...the email and the password should be not null
         Admin admin=(Admin) admindb.findAccount(Email,pass);
         if(admin != null){ //In case admin is not null, system will welcome admin and print out their employees.
             System.out.println("Welcome " + admin.getFirstName());
-            System.out.println("\nYour employee");
-            staff=admindb.retrieveAll();
-            for (int i=0; i<staff.size()-1; i++){
-                System.out.println(staff.get(i));
-            }
             return admin;
         }
         else {System.out.println("Incorrect username or password"); // sign in is failed
             return null;}
-
     }
+
+
     public Admin CreateAdmin(String firstName, String lastName, String email, String password, boolean isGlobal, boolean isActive){
         if(firstName.isEmpty()|| firstName.length()<2){
             System.out.println("Error! Please, Enter a valid name"); // In case firstname is null or less than 2 chracters.
@@ -80,19 +77,17 @@ public class AdminController {
 
 
         }
-        }
+    }
     public boolean DeleteEmployee(int empID,boolean isAdmin){
-        AdminDB adminDB=new AdminDB();
-        StaffDB staffDB = new StaffDB();
-        Admin admin = new Admin();
-        if(adminDB.findAccount(empID)!=null){ // to see if account exists in database
-        if(isAdmin&&admin.isGlobal()){ // to check account is admin and global
-            adminDB.deleteAccount(empID);
-            return true; //admin account is deleted
-        }else if (!isAdmin){
-            staffDB.deleteAccount(empID);
-            return true; // employee account is deleted
-        }else return false; // in case admin account is not global
+        if(admindb.findAccount(empID)!=null){ // to see if account exists in database
+            if(isAdmin && admin.isGlobal()){ // to check account is admin and global
+                admindb.deleteAccount(empID);
+                return true; //admin account is deleted
+            }else if (!isAdmin){
+                staffdb.deleteAccount(empID);
+                return true; // employee account is deleted
+            }
+            else return false; // in case admin account is not global
         }else return false; // in case account is not found
     }
 }
