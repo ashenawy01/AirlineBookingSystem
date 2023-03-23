342654899136aa23269046f129ecc939607756b0
Noureldin <noor.moataz.lashin@gmail.com>
2023-03-23 22:40:29 +0200

    Noureldin213527

--- src/Controller/FlightController.java
+++ src/Controller/FlightController.java
@@ -9,44 +9,46 @@ public class FlightController {
     private FlightDB flightDB=new FlightDB();
     private Flight flight=new Flight();
     ArrayList<Flight> flights=new ArrayList<Flight>();
+    LocalDateTime today= java.time.LocalDateTime.now();
     public ArrayList<Flight> findFlights(String origin, String destination, LocalDateTime flightTime, float duration, ClassType classType){
-        ArrayList<Flight> Addtolist=new ArrayList<Flight>();
-        for (int i=0; i<flights.size(); i++ ){
-            Flight obj = flights.get(i);
-            if(origin==flights.get(i).getOrigin()&&destination==flights.get(i).getDestination()
-                    &&flightTime==flights.get(i).getFlightTime()
-                    &&duration==flights.get(i).getDuration()){
-
-                        Addtolist.add(obj);
-
-            }
-            else continue;
-
-        }
-        if(Addtolist.isEmpty()){
-            for (int i=0; i<flights.size(); i++ ){
+        if (flightTime.isAfter(today)) {
+            ArrayList<Flight> Addtolist = new ArrayList<Flight>();
+            for (int i = 0; i < flights.size(); i++) {
                 Flight obj = flights.get(i);
-                if(flightTime==flights.get(i).getFlightTime()
-                        ||duration==flights.get(i).getDuration()){
+                if (origin == flights.get(i).getOrigin() && destination == flights.get(i).getDestination()
+                        && flightTime == flights.get(i).getFlightTime()
+                        && duration == flights.get(i).getDuration()) {
 
                     Addtolist.add(obj);
 
-                }
-                else continue;
+                } else continue;
 
             }
-        }
+            if (Addtolist.isEmpty()) {
+                for (int i = 0; i < flights.size(); i++) {
+                    Flight obj = flights.get(i);
+                    if (flightTime == flights.get(i).getFlightTime()
+                            || duration == flights.get(i).getDuration()) {
+
+                        Addtolist.add(obj);
+
+                    } else continue;
+
+                }
+            }
 
 
-        if(!Addtolist.isEmpty()){
-            System.out.println("All available flights");
-            for(int i=0; i<Addtolist.size(); i++){
-                System.out.println(Addtolist.get(i));
-            }return Addtolist;
-        }else{
-            System.out.println("Sorry...there is no available Flights ");
-            return null;
-        }
+            if (!Addtolist.isEmpty()) {
+                System.out.println("All available flights");
+                for (int i = 0; i < Addtolist.size(); i++) {
+                    System.out.println(Addtolist.get(i));
+                }
+                return Addtolist;
+            } else {
+                System.out.println("Sorry...there is no available Flights ");
+                return null;
+            }
+        }else {System.out.println("the date you entered is invalid "); return null;}
     }
     public Flight FlightDetails(int flightID) {
         for (int i = 0; i < flights.size(); i++) {
@@ -68,7 +70,7 @@ public class FlightController {
         System.out.println("the id is not found....please try again ");
         return null;
     }
-    public boolean DeleteFlight(int flightID,LocalDateTime newtime){
+    public boolean DeleteFlight(int flightID){
         for (int i = 0; i < flights.size(); i++) {
             if (flightID == flights.get(i).getFlightID()) {
                 flightDB.deleteFlight(flightID);
@@ -79,9 +81,11 @@ public class FlightController {
         return false;
     }
     public Flight AddFlight(String origin, String destination, LocalDateTime flightTime, float duration, double ticketPrice, Airline airline, TreeSet<Seat> seats){
+        if (flightTime.isAfter(today)) {
         Flight flight =new Flight(origin, destination, flightTime, duration, ticketPrice, airline, seats);
         flights.add(flight);
         return flight;
+        }else {System.out.println("the date you entered is invalid "); return null;}
     }
 
 }
