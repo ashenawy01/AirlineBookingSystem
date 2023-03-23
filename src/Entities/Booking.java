849e9470735f5ab3bcb99f4cb419481a61210f27
Alshal <abdallah224005@bue.edu.eg>
2023-03-22 23:48:58 -0700

    Merge remote-tracking branch 'origin/main'

--- src/Entities/Booking.java
+++ src/Entities/Booking.java
@@ -1,4 +1,31 @@
 package Entities;
 
+import java.util.ArrayList;
+import java.util.TreeSet;
+
 public class Booking {
+
+    private int bookingID;
+    private int clintID;
+
+    private date date;
+    private int  travelers;
+    private TreeSet<Flight> flights = new TreeSet<>();
+
+    public int getClintID() {
+        return clintID;
+    }
+
+    public date getDate() {
+        return date;
+    }
+
+    public void setDate(date date) {
+        this.date = date;
+    }
+
+
+    public TreeSet<Flight> getFlights() {
+        return flights;
+    }
 }
