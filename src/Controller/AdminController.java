7e6df0a44cb5ee6f6011ab65c2f1fea5af97d6b7
BUEdevelopers <web.project22cs@outlook.com>
2023-03-23 12:55:48 +0200

    moutasem

--- src/Controller/AdminController.java
+++ src/Controller/AdminController.java
@@ -2,6 +2,9 @@ package Controller;
 import Entities.*;
 import Model.AdminDB;
 import Model.StaffDB;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.ArrayList;
 public class AdminController {
 
@@ -29,15 +32,21 @@ public class AdminController {
     }
 
     public Admin CreateAdmin(String firstName, String lastName, String email, String password, boolean isGlobal, boolean isActive){
+        if(isValid(email)){
         Admin admin = new Admin( firstName,  lastName,  email,  password,  isGlobal,  isActive);
         return admin;
+        }else{
+            return null;}
     }
 
 
     public Staff CreateStaff(String firstName, String lastName, String email, String password, String jobTitle, Department department, ArrayList<Flight> managedFlights){
-
+        if(isValid(email)){
             Staff staff = new Staff( firstName,lastName,email, password, jobTitle, department, managedFlights);
             return staff;
+        }else{
+            System.out.println("Error email is not right");
+            return null;}
     }
 
 
@@ -46,18 +55,27 @@ public class AdminController {
         AdminDB adminDB=new AdminDB();
         StaffDB staffDB = new StaffDB();
         Admin admin = new Admin();
-        if(adminDB.findAccount(empID)!=null){
-        if(isAdmin&&admin.isGlobal()){
+        if(adminDB.findAccount(empID)!=null){ // to see if account exists in database
+        if(isAdmin&&admin.isGlobal()){ // to check account is admin and global
             adminDB.deleteAccount(empID);
-            return true;
+            return true; //admin account is deleted
         }else if (!isAdmin){
             staffDB.deleteAccount(empID);
-            return true;
-        }else return false;
-        }else return false;
+            return true; // employee account is deleted
+        }else return false; // in case admin account is not global
+        }else return false; // in case account is not found
     }
 
 
     //to check the validation of the email
+    public static boolean isValid(String Email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        return pat.matcher(Email).matches();
+    }
 }
 
