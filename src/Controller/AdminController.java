4db9ee16b31e6dd2e6aa21ba420bc5ce4ca381dd
Noureldin <noor.moataz.lashin@gmail.com>
2023-03-23 12:52:21 +0200

    Noureldin213527

--- src/Controller/AdminController.java
+++ src/Controller/AdminController.java
@@ -2,9 +2,6 @@ package Controller;
 import Entities.*;
 import Model.AdminDB;
 import Model.StaffDB;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.ArrayList;
 public class AdminController {
 
@@ -32,21 +29,15 @@ public class AdminController {
     }
 
     public Admin CreateAdmin(String firstName, String lastName, String email, String password, boolean isGlobal, boolean isActive){
-        if(isValid(email)){
         Admin admin = new Admin( firstName,  lastName,  email,  password,  isGlobal,  isActive);
         return admin;
-        }else{
-            return null;}
     }
 
 
     public Staff CreateStaff(String firstName, String lastName, String email, String password, String jobTitle, Department department, ArrayList<Flight> managedFlights){
-        if(isValid(email)){
+
             Staff staff = new Staff( firstName,lastName,email, password, jobTitle, department, managedFlights);
             return staff;
-        }else{
-            System.out.println("Error email is not right");
-            return null;}
     }
 
 
@@ -68,14 +59,5 @@ public class AdminController {
 
 
     //to check the validation of the email
-    public static boolean isValid(String Email) {
-        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
-                "[a-zA-Z0-9_+&*-]+)*@" +
-                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-                "A-Z]{2,7}$";
-
-        Pattern pat = Pattern.compile(emailRegex);
-        return pat.matcher(Email).matches();
-    }
 }
 
