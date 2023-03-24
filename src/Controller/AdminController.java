ed58d3ccf8f8d3103ba3a54b3c726efd637fbc88
Noureldin <noor.moataz.lashin@gmail.com>
2023-03-24 03:00:05 +0200

    Noureldin213527

--- src/Controller/AdminController.java
+++ src/Controller/AdminController.java
@@ -2,12 +2,14 @@ package Controller;
 import Entities.*;
 import Model.AdminDB;
 import Model.StaffDB;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 public class AdminController {
 
     private static AdminDB admindb = new AdminDB();
     private static StaffDB staffdb = new StaffDB();
-    private Admin admin;
+    private static Staff staff=new Staff();
+    private static Admin admin = new Admin();
 
 
     //Function to sign in for only admins
@@ -27,19 +29,23 @@ public class AdminController {
 
 
     public Admin CreateAdmin(String firstName, String lastName, String email, String password, boolean isGlobal, boolean isActive){
-        if(firstName.isEmpty()|| firstName.length()<2){
+        if(firstName.length()<2){
             System.out.println("Error! Please, Enter a valid name"); // In case firstname is null or less than 2 chracters.
             return null;
-        }else if(lastName.isEmpty()|| lastName.length()<2){
+        }else if(lastName.length()<2){
             System.out.println("Error! Please, Enter a valid name"); // In case Lasttname is null or less than 2 chracters.
             return null;
         }else if (email==null){ // In case email is null
             System.out.println("Error! Please, Enter a valid Email");
             return null;
-        } else if (password.isEmpty() || password.length() < 6 || !((password.matches(".*[a-zA-Z].*") && password.matches(".*\\d.*")))) { //In case user didnt input password correctly
+        } else if ( password.length() < 6 || !((password.matches(".*[a-zA-Z].*") && password.matches(".*\\d.*")))) { //In case user didnt input password correctly
             System.out.println("Error! Please, Enter a valid pass (more than 6 char, includes chars and numbers)");
             return null;
-        }else {
+        }else if (!isValid(email)){
+        System.out.println("Error! Please, Enter a valid Email ");
+        return null;
+        }
+            else {
             if (this.admin.isActive()) {
                 Admin admin=new Admin( firstName, lastName, email, password,isGlobal, isActive); // A new admin is added to the database
                 if(admindb.addObject(admin,true)){ // to check admin is added.
@@ -55,11 +61,22 @@ public class AdminController {
         }
         return null;
     }
+
+    public boolean UpdatePassword(int adminID,String oldpass,String newpass){
+        if(admindb.findAccount(adminID)!=null){
+            admin=(Admin) admindb.findAccount(adminID);
+            if (newpass!=admin.getPassword()&&oldpass==admin.getPassword()){
+                admin.setPassword(newpass);
+                return true;
+            }else {System.out.println("the old password or the new password is wrong, please try again "); return false;}
+        } else {System.out.println("please enter a vaild ID "); return false;}
+    }
+
     public Staff CreateStaff(String firstName, String lastName, String email, String password, String jobTitle, Department department){
-        if(firstName.isEmpty()|| firstName.length()<2){ // In case firstname is not null or less than 2 characters
+        if( firstName.length()<2){ // In case firstname is not null or less than 2 characters
             System.out.println("Error! Please, Enter a valid name");
             return null;
-        }else if(lastName.isEmpty()|| lastName.length()<2){ // In case lastName is not null or less than 2 characters
+        }else if(lastName.length()<2){ // In case lastName is not null or less than 2 characters
             System.out.println("Error! Please, Enter a valid name");
             return null;
         }else if (email==null){ // In case email is not null
@@ -69,7 +86,7 @@ public class AdminController {
             System.out.println("Error! Please, Enter a valid Email ");
             return null;
         }
-        else if (password.isEmpty() || password.length() < 6 || !((password.matches(".*[a-zA-Z].*") && password.matches(".*\\d.*")))) { // if password inputed is not valid
+        else if ( password.length() < 6 || !((password.matches(".*[a-zA-Z].*") && password.matches(".*\\d.*")))) { // if password inputed is not valid
             System.out.println("Error! Please, Enter a valid pass (more than 6 char, includes chars and numbers)");
             return null;
         } else {
@@ -97,6 +114,33 @@ public class AdminController {
             else return false; // in case admin account is not global
         }else return false; // in case account is not found
     }
+
+    public boolean BandAdmin(int adminID){
+        if (admindb.findAccount(adminID)!=null){
+            admin = (Admin) admindb.findAccount(adminID);
+            admin.setActive(false);
+            return true;
+        }else {System.out.println("the id you entered is not exist ...please try again"); return false;}
+    }
+
+    public ArrayList<Staff> ListAllStaffs (){
+        ArrayList<Staff> Stf=new ArrayList<Staff>();
+        ArrayList<Object>stf=staffdb.retrieveAll();
+        for(int i=0; i<stf.size()-1; i++){
+            Stf.add((Staff) stf.get(i));
+        }
+        return Stf;
+    }
+
+    public ArrayList<Admin> ListAllAdmins(){
+        ArrayList<Admin> Admn=new ArrayList<Admin>();
+        ArrayList<Object>admn=admindb.retrieveAll();
+        for(int i=0; i<admn.size()-1; i++){
+            Admn.add((Admin) admn.get(i));
+        }
+        return Admn;
+    }
+
 
     //t check email validation
     public static boolean isValid(String email)
