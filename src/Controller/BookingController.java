2243135b545723a399431082b654e09ddf513f0a
abdelrhman <abdelrhman225328@bue.edu.eg>
2023-03-24 07:58:22 +0200

    552

--- src/Controller/BookingController.java
+++ src/Controller/BookingController.java
@@ -56,15 +56,6 @@ public class BookingController {
         return results;
     }
 
-    public static ArrayList<Flight> getAllFlights () {
-        ArrayList<Flight> flightArrayList = new ArrayList<>();
-
-        flightDB.retrieveAll().forEach(flightObj -> {
-            Flight flight = (Flight) flightObj;
-            flightArrayList.add(flight);
-        });
-        return flightArrayList;
-    }
 
     public static Booking CreateBooking(int clintID, LocalDateTime date, int travelers, TreeSet<Flight> flights) {
         if (clientDB.findAccount(clintID) == null) {
@@ -86,58 +77,7 @@ public class BookingController {
         }
     }
 
-    public Flight findFlight(int flightID) {
-        for (int i = 0; i < getAllFlights().size(); i++) {
-            if (flightID == getAllFlights().get(i).getFlightID()) {
-                return getAllFlights().get(i);
-            } else continue;
-        }
-        System.out.println("Sorry...there is no flight with this id :" + flightID);
-        return null;
-    }
 
-    public Flight UpdateFlightTime(int flightID,LocalDateTime newTime){
-        Flight flight = findFlight(flightID);
-        if (flight == null) {
-            System.out.println("the id is not found....please try again ");
-            return null;
-        }
-        flight.setFlightTime(newTime);
-        flightDB.updateFlight(flightID, flight);
-        return flight;
-    }
 
-    public boolean DeleteFlight(int flightID){
-        Flight flight = findFlight(flightID);
-        if (flight == null) {
-            System.out.println("the id is not found....please try again ");
-            return false;
-        }
-        return flightDB.deleteFlight(flightID);
-    }
-    public Flight AddFlight(String origin, String destination, LocalDateTime flightTime, float duration, double ticketPrice, Airline airline, TreeSet<Seat> seats){
-
-        if (origin.isEmpty()) {
-            System.out.println("Error - Enter a valid origin");
-            return null;
-        } else if (destination.isEmpty()) {
-            System.out.println("Error - Enter a valid destination");
-            return null;
-        } else if (flightTime.isBefore(LocalDateTime.now())) {
-            System.out.println("Error - Old date");
-            return null;
-        } else if (duration < 0) {
-            System.out.println("Error - Enter a valid duration");
-            return null;
-        } else if (ticketPrice < 0) {
-            System.out.println("Error - Enter a valid duration");
-            return null;
-        } else {
-            Flight flight = new Flight(origin, destination, flightTime, duration, ticketPrice, airline, seats);
-            flightDB.addObject(flight, true);
-            return flight;
-        }
-
-    }
 
 }
---
+++ src/Controller/FlightController.java
@@ -0,0 +1,91 @@
+package Controller;
+
+import Entities.Airline;
+import Entities.Flight;
+import Entities.Seat;
+import Model.ClientDB;
+import Model.FlightDB;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+public class FlightController {
+    private static FlightDB flightDB=new FlightDB();
+    private static ClientDB clientDB = new ClientDB();
+
+    public static ArrayList<Flight> getAllFlights () {
+        ArrayList<Flight> flightArrayList = new ArrayList<>();
+
+        flightDB.retrieveAll().forEach(flightObj -> {
+            Flight flight = (Flight) flightObj;
+            flightArrayList.add(flight);
+        });
+        return flightArrayList;
+    }
+
+    public static Flight findFlightByID(int flightID) {
+        for (int i = 0; i < getAllFlights().size(); i++) {
+            if (flightID == getAllFlights().get(i).getFlightID()) {
+                return getAllFlights().get(i);
+            } else continue;
+        }
+        System.out.println("Sorry...there is no flight with this id :" + flightID);
+        return null;
+    }
+    public static ArrayList<Flight> findFlightFrom(String origin , LocalDateTime date) {
+        ArrayList<Flight> flights = new ArrayList<>();
+        getAllFlights().forEach(flight -> {
+            if (flight.getOrigin().equalsIgnoreCase(origin) && flight.getFlightTime().getDayOfMonth() == date.getDayOfMonth()) {
+                flights.add(flight);
+            }
+        });
+        return flights;
+    }
+
+    public static Flight UpdateFlightTime(int flightID, LocalDateTime newTime){
+        Flight flight = findFlightByID(flightID);
+        if (flight == null) {
+            System.out.println("the id is not found....please try again ");
+            return null;
+        }
+        flight.setFlightTime(newTime);
+        flightDB.updateFlight(flightID, flight);
+        return flight;
+    }
+
+    public static boolean DeleteFlight(int flightID){
+        Flight flight = findFlightByID(flightID);
+        if (flight == null) {
+            System.out.println("the id is not found....please try again ");
+            return false;
+        }
+        return flightDB.deleteFlight(flightID);
+    }
+    public static Flight AddFlight(String origin, String destination, LocalDateTime flightTime, float duration, double ticketPrice, Airline airline, TreeSet<Seat> seats){
+
+        if (origin.isEmpty()) {
+            System.out.println("Error - Enter a valid origin");
+            return null;
+        } else if (destination.isEmpty()) {
+            System.out.println("Error - Enter a valid destination");
+            return null;
+        } else if (flightTime.isBefore(LocalDateTime.now())) {
+            System.out.println("Error - Old date");
+            return null;
+        } else if (duration < 0) {
+            System.out.println("Error - Enter a valid duration");
+            return null;
+        } else if (ticketPrice < 0) {
+            System.out.println("Error - Enter a valid duration");
+            return null;
+        } else {
+            Flight flight = new Flight(origin, destination, flightTime, duration, ticketPrice, airline, seats);
+            flightDB.addObject(flight, true);
+            return flight;
+        }
+
+    }
+
+
+}
